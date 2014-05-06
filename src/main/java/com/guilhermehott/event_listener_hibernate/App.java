64e861b8613a51d6b88f2e4378dcc256bce3e576
guilherme.hott <guilhermehott@gmail.com>
2014-05-06 15:21:37 -0300

    teste

---
+++ src/main/java/com/guilhermehott/event_listener_hibernate/App.java
@@ -0,0 +1,13 @@
+package com.guilhermehott.event_listener_hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
